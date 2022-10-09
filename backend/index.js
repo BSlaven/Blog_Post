cc9d5c9dff9ba9cfa01656c4388ca7cb06c09726
SlavenB <slaven.bunijevac@gmail.com>
2022-10-09 15:39:12 +0200

    add body-parser and cors into index file

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const postsRouter = require('./routes/posts');
 
