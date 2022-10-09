dc99dde303dbcc15d6d324767739b5f202dc2695
SlavenB <slaven.bunijevac@gmail.com>
2022-10-09 14:55:28 +0200

    add router to fetch one post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const postModel = require('../models/postModel');
+const Post = require('../models/postModel');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   // ovdje tražim sve postove
+});
+
+router.get('/:id', async (req, res) => {
+  // ovdje tražim jedan post sa navedenim id
 });
 
 module.exports = router;
