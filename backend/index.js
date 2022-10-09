15dbbd2ec01c1b512cfe197717702a40889b1043
SlavenB <slaven.bunijevac@gmail.com>
2022-10-09 15:24:53 +0200

    add mongoose connection but keep it commented for now

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,19 @@ const postsRouter = require('./routes/posts');
 
 app.use('posts', postsRouter);
 
+// mongoose
+//   .connect(process.env.MONGO_DEV_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false    
+//   })
+//   .then(console.log("Database connected!"))
+//   .catch(err => console.log(err));
+
+// app.listen(port, function () {
+//   console.log("Runnning on " + port);
+// });
+
 app.listen(port, () => {
   console.log(`Aplikacija radi na portu: ${port}`);
 });
