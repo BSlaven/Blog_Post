1e3adc1b148756f378e03fc50f53bf68047a52c2
SlavenB <slaven.bunijevac@gmail.com>
2022-10-09 15:21:15 +0200

    add posts router in the index so app can use it

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ const app = express();
 
 const port = process.env.PORT || 3001;
 
+const postsRouter = require('./routes/posts');
 
+app.use('posts', postsRouter);
 
 app.listen(port, () => {
   console.log(`Aplikacija radi na portu: ${port}`);
