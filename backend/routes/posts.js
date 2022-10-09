976ee977bda60c412662b81d71f6ba97a90e23e0
SlavenB <slaven.bunijevac@gmail.com>
2022-10-09 14:53:11 +0200

    add router to get all posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,6 +3,8 @@ const postModel = require('../models/postModel');
 
 const router = express.Router();
 
-
+router.get('/', async (req, res) => {
+  // ovdje tražim sve postove
+});
 
 module.exports = router;
