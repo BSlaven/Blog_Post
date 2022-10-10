d0abc21ea4da78f94364f6ee74792d7121847a5a
SlavenB <slaven.bunijevac@gmail.com>
2022-10-11 00:00:10 +0200

    introduce layout component which will render app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,27 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.css'
-import Header from './layout/Header'
+import Layout from './components/Layout';
+import Posts from './components/Posts';
 
 function App() {
 
   return (
     <div className="App">
       <div className="container">
-        <Header />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Posts />} />
+            <Route path='post'>
+              <Route index element={<AddPostForm />} />
+              <Route path=':postId' element={<SinglePostPage />} />
+              <Route path='edit/:postId' element={<EditPostForm />} />
+            </Route>
+
+            {/* catch all - maybe replace with 404 component  */}
+            {/* <Route path='*' element={<Navigate to='/' replace />} /> */}
+          </Route>
+        </Routes>
       </div>
     </div>
   )
