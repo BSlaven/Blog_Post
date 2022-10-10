39498aa93ff663b7d4e38c14fe7498be278cb3f3
SlavenB <slaven.bunijevac@gmail.com>
2022-10-11 00:20:15 +0200

    move header component to components folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,27 +3,24 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
 import Layout from './components/Layout';
 import Posts from './components/Posts';
+import PostPage from './components/PostPage';
 
 function App() {
 
   return (
-    <div className="App">
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Posts />} />
-            <Route path='post'>
-              <Route index element={<AddPostForm />} />
-              <Route path=':postId' element={<SinglePostPage />} />
-              <Route path='edit/:postId' element={<EditPostForm />} />
-            </Route>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Posts />} />
+        <Route path='post'>
+          <Route index element={<PostPage />} />
+          {/* <Route path=':postId' element={<SinglePostPage />} />
+          <Route path='edit/:postId' element={<EditPostForm />} /> */}
+        </Route>
 
-            {/* catch all - maybe replace with 404 component  */}
-            {/* <Route path='*' element={<Navigate to='/' replace />} /> */}
-          </Route>
-        </Routes>
-      </div>
-    </div>
+        {/* catch all - maybe replace with 404 component  */}
+        {/* <Route path='*' element={<Navigate to='/' replace />} /> */}
+      </Route>
+    </Routes>
   )
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PostPage from './PostPage';
+import Posts from './Posts';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      {/* <Link to="/post">One post</Link>
+      <Link to="/">All posts</Link> */}
+    </header>
+  )
+}
+
+export default Header
