66dea5729829ce5cfb099b69d4648a176ae71bb8
SlavenB <slaven.bunijevac@gmail.com>
2022-10-15 23:21:00 +0200

    remove react-markdown and add editor.js

--- frontend/src/components/NewPost.jsx
+++ frontend/src/components/NewPost.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import ReactMarkdown from 'react-markdown';
 
 const NewPost = () => {
   return (
-    <ReactMarkdown>
-      # Hello, *world*!
-    </ReactMarkdown>
-    // <div>Ovo je stranica za novi post.</div>
+    <div>Ovo je stranica za novi post.</div>
   )
 }
 
