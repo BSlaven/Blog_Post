16cf86e30a4c07f3a901312a074fe3dc65182816
SlavenB <slaven.bunijevac@gmail.com>
2022-10-17 22:37:47 +0200

    add content, title, description elements in the post form

--- frontend/src/components/NewPost.jsx
+++ frontend/src/components/NewPost.jsx
@@ -7,8 +7,19 @@ const NewPost = () => {
     <div className="form-container">
       <h3 className='form-title'>Create new post</h3>
       <form className="form">
-        <input type="text" name="post-title" id="post-title" />
-        <DraftEditor />
+        <div className="form-control">
+          <label htmlFor="post-title">Title</label>
+          <input type="text" name="post-title" id="post-title" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="post-description">Description</label>
+          <input type="text" name="post-description" id="post-description" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="post-content">Content</label>
+          <DraftEditor />
+        </div>
+        <button type='submit'>Save post</button>
       </form>
     </div>
   )
