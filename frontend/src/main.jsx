9c254fd75edf7fe5b038fe6937ea27954f2297e9
SlavenB <slaven.bunijevac@gmail.com>
2022-10-25 00:21:57 +0200

    remove routing imports from main.jsx file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,20 +4,15 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { fetchAllPosts } from './store/slices/postsSlice';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './index.css'
 
 store.dispatch(fetchAllPosts());
+console.log('main.jsx file rendered')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <Router>
-        <Routes>
-          <Route to='/' element={<App />} />
-        </Routes>
-      </Router> */}
       <App />
     </Provider>
   </React.StrictMode>
