553482515cc5c3d4ccd715436ac3ae997695dc08
SlavenB <slaven.bunijevac@gmail.com>
2022-10-26 23:56:21 +0200

    set up navigation to go to the post page

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const PostCard = ({ _id: id, title, body }) => {
 
+  const navigate = useNavigate();
+
   const clickCardHandler = e => {
     console.log(`Kliknuo si na post ${id}`);
+    navigate('/post')
   }
 
   return (
