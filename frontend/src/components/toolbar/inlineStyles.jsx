0a3b385a4feba641fbe589dbb74fe89b04261d81
SlavenB <slaven.bunijevac@gmail.com>
2022-10-30 12:09:38 +0100

    add iconst to inline styles

--- frontend/src/components/toolbar/inlineStyles.jsx
+++ frontend/src/components/toolbar/inlineStyles.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBold, faUnderline, faItalic } from "@fortawesome/free-solid-svg-icons";
 
 const inlineStyles = [
   {
