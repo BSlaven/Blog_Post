149de44ee2519051fdcaee5d61f61921159e20bb
SlavenB <slaven.bunijevac@gmail.com>
2022-10-30 12:17:15 +0100

    import inline styles componetn in toolbar

--- frontend/src/components/toolbar/EditorToolbar.jsx
+++ frontend/src/components/toolbar/EditorToolbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+
+import InlineStylesComponent from './InlineStylesComponent';
 
 const EditorToolbar = () => {
   return (
-    <div>EditorToolbar</div>
+    <div>
+      <InlineStylesComponent />
+      {/* <BlockStylesComponent /> */}
+    </div>
   )
 }
 
