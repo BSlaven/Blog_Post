52642aa97e578f47d1f8ade79f05853c6251c565
SlavenB <slaven.bunijevac@gmail.com>
2022-10-30 17:55:58 +0100

    remove testing duplicate component

--- frontend/src/components/toolbar/ToolbarItem.jsx
+++ frontend/src/components/toolbar/ToolbarItem.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const ToolbarItem = ({ isactive, children }) => {
+
+  console.log(isactive)
+  
   return (
-    <div className={`toolbar-item ${isactive ? 'active' : ''}`}>
+    <div className={`toolbar-item ${isactive === 'true' ? 'active' : null}`}>
       {children}
     </div>
   )
