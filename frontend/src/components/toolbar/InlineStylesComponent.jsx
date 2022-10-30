82a5234618ebe97c75a76508c84322a84512b7c4
SlavenB <slaven.bunijevac@gmail.com>
2022-10-30 12:15:15 +0100

    map through inline styles in component

--- frontend/src/components/toolbar/InlineStylesComponent.jsx
+++ frontend/src/components/toolbar/InlineStylesComponent.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import { inlineStyles } from './inlineStyles';
+
 const InlineStylesComponent = () => {
   return (
     <div className='inline-styles-container'>
-      
+      {inlineStyles.map((elem, index) => (
+        <p>{`SB-${index}`}</p>
+      ))}
     </div>
   )
 }
