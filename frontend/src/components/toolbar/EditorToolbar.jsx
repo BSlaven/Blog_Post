ba0108de6bfe6cc599eaf0c9dcbb09350919f414
SlavenB <slaven.bunijevac@gmail.com>
2022-10-31 01:03:01 +0100

    add BlockStylesComponent to toolbar

--- frontend/src/components/toolbar/EditorToolbar.jsx
+++ frontend/src/components/toolbar/EditorToolbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import InlineStylesComponent from './InlineStylesComponent';
+import BlockStylesComponent from './BlockStylesComponent';
 
 const EditorToolbar = ({ editorState, updateEditorState }) => {
   return (
     <div className='toolbar-container'>
       <InlineStylesComponent editorState={editorState} updateEditorState={updateEditorState} />
-      {/* <BlockStylesComponent /> */}
+      <BlockStylesComponent />
     </div>
   )
 }
