34a8f6ddccf3a095286ce9fb165b5e2590a30830
SlavenB <slaven.bunijevac@gmail.com>
2022-11-04 19:17:48 +0100

    inistall draft-js-export-html package

--- frontend/src/components/ArticlePreview.jsx
+++ frontend/src/components/ArticlePreview.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { getCurrentArticle } from '../store/slices/editorSlice';
+import { ContentBlock } from 'draft-js';
 
 const ArticlePreview = () => {
 
   const article = useSelector(state => getCurrentArticle(state));
-  console.log(article)
+  console.log(article);
   
   return (
-    <div>slaven</div>
+    <div></div>
   )
 }
 
