acb6d55ad96f75e2e085daa6f3446a4d14d37a2a
SlavenB <slaven.bunijevac@gmail.com>
2022-11-09 14:00:12 +0100

    import changet method name

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-import { allPosts, fetchAllPostsStatus } from '../store/slices/postsSlice';
+import { allPosts, fetchRequestStatus } from '../store/slices/postsSlice';
 
 import PostCard from './PostCard';
 
 const Posts = () => {
 
   const posts = useSelector(allPosts);
-  const status = useSelector(fetchAllPostsStatus);
+  const status = useSelector(fetchRequestStatus);
 
   console.log(posts)
 
