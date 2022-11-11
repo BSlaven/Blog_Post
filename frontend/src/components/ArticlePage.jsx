f00b9cc6bc8af057aa7d3c47feb88d198b51c207
SlavenB <slaven.bunijevac@gmail.com>
2022-11-11 20:26:05 +0100

    add class name to block container

--- frontend/src/components/ArticlePage.jsx
+++ frontend/src/components/ArticlePage.jsx
@@ -9,13 +9,14 @@ const ArticlePage = () => {
   const { id } = useParams();
 
   const post = useSelector(state => getPostById(state, id));
+  console.log(post)
 
   return (
     <div className="article-container">
       {post && <h1>{post.title}</h1>}
       {post && <h3>{post.description}</h3>}
       {post && JSON.parse(post.body).map(block => (
-        <div key={block.text}>
+        <div key={block.text} className="block-container">
           {block.text}
         </div>
       ))}
