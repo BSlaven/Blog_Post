d852810b84f3f40b5988329b1f1a493355ddae71
SlavenB <slaven.bunijevac@gmail.com>
2022-11-11 22:48:02 +0100

    loop through inline styles to get the classes

--- frontend/src/helpers/parseHelper.js
+++ frontend/src/helpers/parseHelper.js
@@ -3,5 +3,18 @@ export const parseHtmlContent = block => {
 }
 
 export const parseClasses = inlineStyles => {
-  
+  const classes = [];
+  inlineStyles.forEach(item => {
+    switch(item) {
+      case 'BOLD':
+        classes.push('bold');
+        break;
+      case 'ITALIC':
+        classes.push('italic');
+        break;
+      default:
+        classes.push('underline');
+    }
+  })
+  return classes.join(' ');
 }
